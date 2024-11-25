4621d0deb4a54b75e3ab1b247d0b2a0065a7dcca
John Rocha <xjohnfitcodes@gmail.com>
2024-11-25 12:43:15 -0500

    HeartDiv

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Auth from "./Auth";
+
+function App() {
+  return (
+    <>
+      <Auth />
+    </>
+  )
+}
+
+export default App
---
+++ src/Auth.jsx
@@ -0,0 +1,11 @@
+const Auth = () => {
+
+    return (
+        <div className="flex justify-center items-center h-[100vh]">
+            <div className="h-[400px] w-[400px] bg-red-600 -rotate-[135deg] relative before:content-[''] before:h-[400px] before:w-[400px] before:bg-red-600 before:rounded-full before:absolute after:content-[''] before:top-[200px] before:left-0 after:h-[400px] after:w-[400px] after:bg-red-600 after:rounded-full after:absolute after:left-[200px] after:top-0">
+
+            </div>
+        </div>
+    );
+};
+export default Auth;
